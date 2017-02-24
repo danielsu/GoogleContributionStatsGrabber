1e62e8ebca83db212a41193762e2485b36ef9d8f
Daniel Suess <daniel.suess@akquinet.de>
2017-02-24 11:42:07 +0100

    Grab views of multiple photos correctly

--- GoogleContributionStatsGrabber.bookmark.js
+++ GoogleContributionStatsGrabber.bookmark.js
@@ -1,22 +1,8 @@
-/*
- Google Contribution Stats Grabber
-
- .section-photo-bucket-content		(one place, with title and multiple photos)
-     .section-photo-bucket-title			(Name)
-     .section-photo-bucket-subtitle	(Address)
-     .section-photo-bucket-photos		(multiple photos possible)
-        .section-photo-bucket-photo-container
-        .maps-sprite-photos-view-count		(Number of Views)
- */
-
-
-(function grabStats() {
-    var grabBox = document.createElement('div'),
-        closeButton = document.createElement('button'),
-        countVisibleItemsText = document.createElement('div'),
-        countExtractedItemsText = document.createElement('div'),
-        resultBox = document.createElement('textarea');
-
+javascript:(function grabStats() {
+    var grabBox = document.createElement('div');
+    var closeButton = document.createElement('button');
+    var countVisibleItemsText = document.createElement('div');
+    var countExtractedItemsText = document.createElement('div');
     grabBox.setAttribute('style', 'position: absolute; top: 10px; left: 260px; background:rgba(175, 175, 175, 0.9); padding: 0.5em; box-shadow: 5px 5px 20px #6f6f6f;');
     grabBox.setAttribute('id', 'grabBox');
     closeButton.setAttribute('style', ' height: 2em; width: 2em; position: absolute; top: 0; right: 0;');
@@ -24,24 +10,20 @@
     closeButton.addEventListener('click', function () {
         document.body.removeChild(grabBox)
     });
-
     grabBox.appendChild(closeButton);
     countVisibleItemsText.setAttribute('style', ' height: 2em; display: inline-block;');
     grabBox.appendChild(countVisibleItemsText);
     countExtractedItemsText.setAttribute('style', ' height: 2em; display: inline-block; margin-left: 2em');
     grabBox.appendChild(countExtractedItemsText);
-
+    var resultBox = document.createElement('textarea');
     resultBox.setAttribute('style', 'width: 900px; height: 200px; display:block; border-top: 1px solid black;');
     resultBox.setAttribute('id', 'resultBox');
     grabBox.appendChild(resultBox);
-
     extractData();
     document.querySelector('.section-scrollbox').addEventListener('DOMSubtreeModified', function (propname, oldValue, newValue) {
         extractData()
     }, false);
-    /*final step: append box to DOM*/
-    document.body.appendChild(grabBox);
-
+    document.body.append(grabBox);
     function extractData() {
         var resultArray = [];
         resultArray.push('title\taddress\tviews\tsource');
